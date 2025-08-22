5fd26da2c917459740958dbe3e9e940247122f04
anjaducic <ducic.ra5.2020@uns.ac.rs>
2025-08-23 00:50:21 +0200

    Filtriranje presuda - postojece/generisane

--- PravnaInformatikaFE/src/components/verdict-list/verdict-list.component.ts
+++ PravnaInformatikaFE/src/components/verdict-list/verdict-list.component.ts
@@ -1,30 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { VerdictService } from '../../services/verdict.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-verdict-list',
   templateUrl: './verdict-list.component.html',
   standalone: true,
   styleUrls: ['./verdict-list.component.css'],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
 })
 export class VerdictListComponent implements OnInit {
 
   verdicts: string[] = [];
+  filteredVerdicts: string[] = [];
+  selectedFilter: 'all' | 'existing' | 'generated' = 'all';
 
   constructor(private verdictService: VerdictService) {}
 
   ngOnInit(): void {
-    // vraca listu fajlova sa backend-a
     this.verdictService.getVerdictFiles().subscribe(list => {
       this.verdicts = list;
+      this.applyFilter();
     });
   }
 
-  // otvara HTML fajl u novom tabu
   openVerdict(fileName: string): void {
     const url = this.verdictService.getVerdictFileUrl(fileName);
     window.open(url, '_blank');
+  }
+
+  applyFilter(): void {
+    if (this.selectedFilter === 'all') {
+      this.filteredVerdicts = [...this.verdicts];
+    } else if (this.selectedFilter === 'existing') {
+      this.filteredVerdicts = this.verdicts.filter(f => !f.startsWith('G'));
+    } else if (this.selectedFilter === 'generated') {
+      this.filteredVerdicts = this.verdicts.filter(f => f.startsWith('G'));
+    }
   }
 }
