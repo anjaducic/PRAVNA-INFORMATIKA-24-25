7c3b555cd2d6cfa9d5097273980b93fa2460bb80
anjaducic <ducic.ra5.2020@uns.ac.rs>
2025-08-25 14:38:10 +0200

    Prikaz metapodataka zakona

--- PravnaInformatikaFE/src/app/app.component.ts
+++ PravnaInformatikaFE/src/app/app.component.ts
@@ -3,17 +3,35 @@ import { RouterModule } from '@angular/router';
 import { VerdictComponent } from '../components/verdict/verdict.component';
 import { LandingPageComponent } from '../components/landing-page/landing-page.component';
 import { MenuComponent } from '../components/menu/menu.component'
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, LandingPageComponent, MenuComponent],
+  imports: [RouterModule, LandingPageComponent, MenuComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'PravnaInformatikaFE';
+
+ lawData: any = null; // metapodaci zakona
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+      this.http.get('laws/laws.csv', { responseType: 'text' }).subscribe(data => {
+        const rows = data.split('\n').map(row => row.split(','));
+        const headers = rows[0];
+        const values = rows[1];
+        this.lawData = headers.reduce((obj: any, key: string, index: number) => {
+          obj[key.trim()] = values[index]?.trim();
+          return obj;
+        }, {});
+      });
+    }
 
   openKrivicni() {
     // sa putanje 'public/laws/krivicni.html'  
