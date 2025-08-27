ea6b6da82c2c3fa91b70e8024b695d797c8fb4cd
Anja <anja.lovric@gmail.com>
2025-08-28 00:26:41 +0200

    fix: cors policy fixed

--- PravnaInformatikaBE/src/main/java/com/example/pravnaInformatika/backend/Config/SecurityConfig.java
+++ PravnaInformatikaBE/src/main/java/com/example/pravnaInformatika/backend/Config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173")); // ovo je za vue.js url
+        configuration.setAllowedOrigins(List.of("http://localhost:4200")); 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
         configuration.setAllowCredentials(true);
